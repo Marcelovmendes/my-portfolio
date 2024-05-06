43f0c87b8457aa4fd1946ba9201913dad40b9fc4
unknown <Marcelovmendes>
2024-05-05 21:29:22 -0300

    feat: add linguage toggle interation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Header } from "@/components/hearder";
+import { LanguageProvider } from "@/contexts/linguage-context";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="h-screen w-screen">
-      <Header />
+      <LanguageProvider>
+        <Header />
+      </LanguageProvider>
     </main>
   );
 }
--- src/components/linguage-toggle.tsx
+++ src/components/linguage-toggle.tsx
@@ -1,10 +1,26 @@
 "use client";
-
+import { useContext } from "react";
+import { LanguageContext, LANGUAGES } from "../contexts/linguage-context";
 export const LanguageToggle = () => {
+  const { language, setLanguage } = useContext(LanguageContext);
   return (
     <div className="flex bg-[#1e1852] items-center justify-center h-24 w-[40%]">
-      <button className="text-white">EN</button>
-      <button className="text-white">BR</button>
+      <button
+        className={`text-white ${
+          language === LANGUAGES.EN ? "focus:outline-none" : "opacity-50"
+        }`}
+        onClick={() => setLanguage(LANGUAGES.EN)}
+      >
+        EN
+      </button>
+      <button
+        className={`text-white ${
+          language === LANGUAGES.PTBR ? "focus:outline-none" : "opacity-50"
+        }`}
+        onClick={() => setLanguage(LANGUAGES.PTBR)}
+      >
+        BR
+      </button>
     </div>
   );
 };
